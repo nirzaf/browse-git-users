39d0699a9ca56b8267d5b58b0891f28bfb437ea6
nirzaf <mfmfazrin1986@gmail.com>
2022-03-13 13:08:28 +0530

    stream

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from "../../environments/environment";
 import { HttpClient } from "@angular/common/http";
 import { UserObject } from "../view-users/view-users.component";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
-  getGitUsersProfile(since:number,perPage:number){
+  getGitUsersProfile(since:number,perPage:number):Observable<UserObject[]> {
     return this.http.get<UserObject[]>(this.apiUrl + '?since=' + since + '&per_page=' + perPage);
   }
 }
